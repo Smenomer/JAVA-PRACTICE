58a80f3d1ee5c3efb5afdb6da33700330f830b99
Sujay Joshi <145902128+Smenomer@users.noreply.github.com>
2026-07-04 17:50:28 +0530

    Add files via upload

---
+++ Strings/comparing.java
@@ -0,0 +1,18 @@
+package smenomer;
+
+public class comparing {
+    public static void main(String[] args) {
+        String a="Sujay";
+        String b="Sujay";
+
+
+        String string1=new String("Sujay");
+        String string2=new String("Sujay");
+        System.out.println(a==b);
+        System.out.println(string1==string2);
+
+
+        System.out.println(string1.equals(string2));
+    }
+}
+
